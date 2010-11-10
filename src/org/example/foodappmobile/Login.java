6f7987f03443cf0ed4ff0df67fc0cac4b9d5cbdd
c4l3f86 <caleffi.enrico@gmail.com>
2010-11-10 17:11:07 +0100

    codice per visualizzare il codice della risposta http

--- src/org/example/foodappmobile/Login.java
+++ src/org/example/foodappmobile/Login.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
@@ -12,6 +13,9 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.CoreProtocolPNames;
+import org.apache.http.params.HttpParams;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -37,15 +41,20 @@ public class Login extends Activity{
     
     public void postData(){
     	
+    	//Serve per fare in modo che il metodo POST venga gestito tramite la 
+    	// versione di HTTP 1.1; in questo modo la risposta è molto più performante
+    	HttpParams params = new BasicHttpParams();
+    	params.setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
+    	
     	//Crea un nuovo HttpClient e POST Header
-    	HttpClient httpclient = new DefaultHttpClient();
+    	HttpClient httpclient = new DefaultHttpClient(params);
     	HttpPost httppost = new HttpPost("http://192.168.2.6:3000/people/login");
     	
     	final EditText txtName = (EditText)findViewById(R.id.login_name);
     	final EditText txtPw = (EditText)findViewById(R.id.login_pw);
     	
     	try {  
-            // Add your data  
+            // Aggiungo i parametri da passare con la richiesta POST 
             List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);  
             nameValuePairs.add(new BasicNameValuePair("name", txtName.getText().toString()));  
             nameValuePairs.add(new BasicNameValuePair("password", txtPw.getText().toString()));  
@@ -53,7 +62,8 @@ public class Login extends Activity{
       
             // Execute HTTP Post Request  
             HttpResponse response = httpclient.execute(httppost);
-            System.out.println(response);
+            // Se tutto va bene viene ritornato il codice 200
+            System.out.println(response.getStatusLine().getStatusCode());
               
         } catch (ClientProtocolException e) {            
         } catch (IOException e) {  }
